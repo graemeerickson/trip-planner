79b924222053dc45b871f23cf15ec4212ed8e24f
taihelsel <tai.bayard@gmail.com>
2018-06-01 13:00:45 -0700

    Added map component and routeplan component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 //React components
-import Map from "./components/Map/Map.js";
+import RoutePlan from "./components/RoutePlan/RoutePlan.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Map />
+        <RoutePlan />
       </div>
     );
   }
---
+++ src/components/RoutePlan/RoutePlan.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+//React components
+import Map from ".././Map/Map.js";
+
+class RoutePlan extends Component {
+  render() {
+    return (
+        <section id="RoutePlan">
+            <Map />
+        </section>
+    );
+  }
+}
+
+export default RoutePlan;
