e378c6bd75b1a2194d84265a59abd5a075cb90be
taihelsel <tai.bayard@gmail.com>
2018-06-01 14:17:25 -0700

    linked forms with map

--- src/components/RoutePlan/RoutePlan.js
+++ src/components/RoutePlan/RoutePlan.js
@@ -3,17 +3,149 @@ import React, { Component } from 'react';
 //React components
 import RouteForm from ".././RouteForm/RouteForm.js";
 import Map from ".././Map/Map.js";
-
+var map = null;
+var boxpolys = null;
+var directions = null;
+var routeBoxer = null;
+var distance = null;
+var directionService = null;
+var directionsRenderer = null;
+var infoWindow = new window.google.maps.InfoWindow();
+var service = null;
 class RoutePlan extends Component {
   constructor() {
     super()
     this.state = {
       origin: "",
       dest: "",
-      radius: ""
+      radius: "",
+      miles: true,
+    }
+  }
+  componentDidMount() {
+    this.initialize();
+  }
+  // Clear boxes currently on the map
+  clearBoxes = () => {
+    if (boxpolys != null) {
+      for (var i = 0; i < boxpolys.length; i++) {
+        boxpolys[i].setMap(null);
+      }
+    }
+    boxpolys = null;
+  }
+  drawBoxes = (boxes) => {
+    boxpolys = new Array(boxes.length);
+    for (var i = 0; i < boxes.length; i++) {
+      boxpolys[i] = new window.google.maps.Rectangle({
+        bounds: boxes[i],
+        fillOpacity: 0,
+        strokeOpacity: 1.0,
+        strokeColor: '#000000',
+        strokeWeight: 1,
+        map: map
+      });
+    }
+  }
+  route = () => {
+    // Clear any previous route boxes from the map
+    this.clearBoxes();
+
+    if (this.state.miles === true) {
+      // Convert the distance to box around the route from miles to km            
+      distance = parseFloat(this.state.radius) * 1.609344;
+    } else {
+      //Use KM
+      distance = parseFloat(this.state.radius);
+    }
+
+    var request = {
+      origin: this.state.origin,
+      destination: this.state.dest,
+      travelMode: window.google.maps.DirectionsTravelMode.DRIVING
+    }
+    // Make the directions request
+    directionService.route(request, (result, status) => {
+      if (status == window.google.maps.DirectionsStatus.OK) {
+        directionsRenderer.setDirections(result);
+
+        // Box around the overview path of the first route
+        var path = result.routes[0].overview_path;
+        var boxes = routeBoxer.box(path, distance);
+        console.log(boxes);
+        //this.drawBoxes(boxes);
+        this.searchBounds(boxes);
+      } else {
+        alert("Directions query failed: " + status);
+      }
+    });
+  }
+  initialize = () => {
+    // Default the map view to the continental U.S.
+    var mapOptions = {
+      center: new window.google.maps.LatLng(37.09024, -95.712891),
+      mapTypeId: window.google.maps.MapTypeId.ROADMAP,
+      zoom: 4
+    };
+    map = new window.google.maps.Map(document.getElementById("map"), mapOptions);
+    routeBoxer = new window.RouteBoxer();
+    directionService = new window.google.maps.DirectionsService();
+    directionsRenderer = new window.google.maps.DirectionsRenderer({ map: map });
+    service = new window.google.maps.places.PlacesService(map);
+  }
+  searchBounds = (bound) => {
+    for (let i = 0; i < bound.length; i++) {
+      ((i) => {
+        setTimeout(() => {
+          this.performSearch(bound[i]);
+        }, i * 400);
+      })(i);
+    }
+  }
+  performSearch = (bounds) => {
+    console.log("bounds", bounds);
+    var request = {
+      bounds: bounds,
+      keyword: 'things to do'
+    };
+    service.radarSearch(request, this.callback);
+  }
+
+  callback = (results, status) => {
+    if (status !== window.google.maps.places.PlacesServiceStatus.OK) {
+      console.error(status);
+      return;
+    }
+    for (var i = 0, result; result = results[i]; i++) {
+      this.addMarker(result);
     }
   }
 
+  addMarker = (place) => {
+    var marker = new window.google.maps.Marker({
+      map: map,
+      position: place.geometry.location,
+      icon: {
+        url: 'https://developers.google.com/maps/documentation/javascript/images/circle.png',
+        anchor: new window.google.maps.Point(10, 10),
+        scaledSize: new window.google.maps.Size(10, 17)
+      }
+    });
+
+    window.google.maps.event.addListener(marker, 'click', () => {
+      var request = { placeId: place.place_id };
+
+      service.getDetails(request, (result, status) => {
+        if (status !== window.google.maps.places.PlacesServiceStatus.OK) {
+          console.error(status);
+          return;
+        }
+        infoWindow.setContent(result.name);
+        infoWindow.open(map, marker);
+      });
+    });
+    console.log(place);
+  }
   getTripLocations = (originLocation, destLocation, distanceRadius) => {
     console.log('originLocation:', originLocation)
     console.log('destLocation:', destLocation)
@@ -22,14 +154,15 @@ class RoutePlan extends Component {
       origin: originLocation,
       dest: destLocation,
       radius: distanceRadius
-    })
+    });
+    this.route();
   }
 
   render() {
     return (
       <section id="RoutePlan">
-          <RouteForm updateTripLocations={this.getTripLocations} />
-          <Map tripOrigin={this.state.origin} tripDest={this.state.dest} tripRadius={this.state.radius} destLocation={this.state.dest}/>
+        <RouteForm updateTripLocations={this.getTripLocations} />
+        <Map miles={this.state.miles} tripOrigin={this.state.origin} tripDest={this.state.dest} tripRadius={this.state.radius} destLocation={this.state.dest} />
       </section>
     );
   }
